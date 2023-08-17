a09b9b0a0a10a6b86b8decd6245d30c608b45944
Sean Abi Saab <98626715+seanabisaab@users.noreply.github.com>
2023-08-17 16:56:00 -0700

    Add files via upload

---
+++ main.h
@@ -0,0 +1,22 @@
+#define SEC_LED     PORTEbits.RE2       // Defines SEC_LED as PORTE 2
+#define NS_RED      PORTEbits.RE0       // Defines NS_RED as PORTE 0
+#define NS_GREEN    PORTEbits.RE1       // Defines NS_GREEN as PORTE 1
+
+#define NSLT_RED    PORTDbits.RD0       // Defines NS_LT RED as PORTD 0
+#define NSLT_GREEN  PORTDbits.RD1       // Defines NS_LT GREEN as PORTD 1
+
+#define EW_RED      PORTBbits.RB3       // Defines EW_RED as PORTB 3
+#define EW_GREEN    PORTBbits.RB4       // Defines EW_GREEN as PORTB 4
+
+#define EWLT_RED    PORTAbits.RA5       // Defines EWLT_RED as PORTA 5
+#define EWLT_GREEN  PORTCbits.RC0       // Defines EWLT_GREEN as PORTC 0
+
+#define NS_LT_SW    PORTAbits.RA2       // Defines NS_LT_SW as PORTA 2
+#define NS_PED_SW   PORTAbits.RA4       // Defines NS_PED_SW as PORTA 4
+
+#define EW_LT_SW    PORTAbits.RA1       // Defines EW_LT_SW as PORTA 1
+#define EW_PED_SW   PORTAbits.RA3       // Defines EW_PED_SW as PORTA 3
+
+#define MODE_LED    PORTDbits.RD7       // Defines MODE_LED as PORTD 7
+
+
